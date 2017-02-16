93e3e4f7330163c0a17079fc3c8aecc95c5c3ef1
DAVID DE SOUZA CARDOSO <RM76860@fiap.com.br>
2017-02-16 20:52:04 -0200

    Log4J

--- digital/03-DesignPatterns/src/br/com/fiap/property/PropertySingleton.java
+++ digital/03-DesignPatterns/src/br/com/fiap/property/PropertySingleton.java
@@ -3,7 +3,11 @@ package br.com.fiap.property;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class PropertySingleton {
+	private static Logger log = LoggerFactory.getLogger(PropertySingleton.class);
 
 	private static Properties prop;
 	
@@ -12,6 +16,7 @@ public class PropertySingleton {
 	
 	public static Properties getInstance() {
 		if (prop == null) {
+			log.info("Instanciando as Properties");
 			prop = new Properties();
 			try {
 				prop.load(PropertySingleton.class.getResourceAsStream("/config.properties"));
@@ -20,6 +25,7 @@ public class PropertySingleton {
 				e.printStackTrace();
 			}
 		}
+		log.debug("Lendo uma propriedade do PropertySingleton");
 		return prop;
 	}
 }
